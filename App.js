c16f2f928b26e32876fb136b9a76b57658af5113
Thays Ribeiro <94051605+ThayRibeiro0@users.noreply.github.com>
2023-08-09 20:53:34 -0700

    Update App.js

--- App.js
+++ App.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import {comments} from './commentData'
+
+function App(){
+  return 
+  <div> 
+    {comments.map(comment => <Card commentObject = {comment}/>)}
+  </div>
+}
