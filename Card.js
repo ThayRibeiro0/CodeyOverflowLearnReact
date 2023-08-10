b5d78aea8b5dd09db2ad4472d7c43e1b6ec02196
Thays Ribeiro <94051605+ThayRibeiro0@users.noreply.github.com>
2023-08-09 21:36:53 -0700

    Update Card.js

--- Card.js
+++ Card.js
@@ -6,7 +6,7 @@ function Card(props){
   return (
     <>
       <Header profileImg = {props.commentObject.profileImg} username = {props.commentObject.username} />
-      <Body comment={props.commentObject.comment} />
+      <Body comment={props.commentObject.comment}/>
     </>    
   );
 };
