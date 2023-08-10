f82938b2bd8e55d32bbe90834bd42499c8c09000
Thays Ribeiro <94051605+ThayRibeiro0@users.noreply.github.com>
2023-08-09 21:18:30 -0700

    Update Header.js

--- Header.js
+++ Header.js
@@ -3,10 +3,10 @@ import React from 'react';
 function Header(props){
   return (
     <>
-      <img src={props.profileImg}/>
+      <img src={props.profileImg}></img>
       <h1>{props.username}</h1>
     </>
   );
-};
+}
 
 export default Header;
