d94909228cc7efbf4f986e2a027a3a1d5e946ab1
Thays Ribeiro <94051605+ThayRibeiro0@users.noreply.github.com>
2023-08-09 21:17:45 -0700

    Update App.js

--- App.js
+++ App.js
@@ -3,10 +3,15 @@ import {comments} from './commentData';
 import Card from './Card';
 
 function App(){
-  return 
-  <div> 
-    {comments.map(comment => <Card commentObject = {comment}/>)}
-  </div>
+  return (
+    <div> 
+      {
+        comments.map(comment => 
+        <Card commentObject = {comment}/>
+        )
+      }
+    </div>
+  )
 }
 
 export default App;
